8ec47b278a4577eed60e668d7b00567d9a045995
Marek Onuszko <marek.onuszko@gmail.com>
2018-01-12 18:33:58 +0100

    admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BlogPost, Comment, Author, AuthorBio
+
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    readonly_fields = ('pub_date',)
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    inlines = [
+            CommentInline,
+            ]
+
+
+class AuthorBioInline(admin.TabularInline):
+    model = AuthorBio
+
+class BlogPostInline(admin.TabularInline):
+    model = BlogPost
+    readonly_fields = ('pub_date',)
+    show_change_link = True
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = [
+            AuthorBioInline,
+            BlogPostInline,
+            ]
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Author(User):
@@ -16,7 +17,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=300)
     content = models.TextField(max_length=5000)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateTimeField(default=timezone.now, editable=False)
 
     def __str__(self):
         return self.title
@@ -32,7 +33,7 @@ class Comment(models.Model):
     blogpost = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
     content = models.TextField(max_length=1000)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateTimeField(default=timezone.now, editable=False)
 
     def __str__(self):
         return "{0} {1}".format(self.blogpost, self.author)
