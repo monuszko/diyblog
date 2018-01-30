08f0315a2ec38ca3a33316f963560bb753856152
Marek Onuszko <marek.onuszko@gmail.com>
2018-01-30 17:03:30 +0100

    simple tests for public views

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -33,3 +33,82 @@ class IndexViewTest(TestCase):
         self.assertTrue(resp.context['num_authors'] == 13)
         self.assertTrue(resp.context['num_posts'] == 13 * 19)
         self.assertEquals(resp.context['num_comments'], 13 * 19 * 13)
+
+
+class BlogPostListTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        for anum in range(13):
+            a = factories.AuthorFactory()
+            factories.BioFactory(author=a)
+            for bpnum in range(19):
+                bp = factories.BlogPostFactory(author=a)
+
+    def test_view_exists_at_desired_location(self):
+        resp = self.client.get('/blog/posts/')
+        self.assertEqual(resp.status_code, 200)
+
+
+    def test_view_accessible_by_name(self):
+        resp = self.client.get(reverse('blog:blogpost_list'))
+        self.assertEqual(resp.status_code, 200)
+
+
+class BlogPostDetailTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        for anum in range(13):
+            a = factories.AuthorFactory()
+            factories.BioFactory(author=a)
+            for bpnum in range(19):
+                bp = factories.BlogPostFactory(author=a)
+
+        for u in User.objects.all():
+            for bp in BlogPost.objects.all():
+                factories.CommentFactory(author=u, blogpost=bp)
+
+    def test_view_exists_at_desired_location(self):
+        resp = self.client.get('/blog/3/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_accessible_by_name(self):
+        resp = self.client.get(reverse('blog:post_detail', args=[3]))
+        self.assertEqual(resp.status_code, 200)
+
+
+class AuthorListTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        for anum in range(13):
+            a = factories.AuthorFactory()
+            factories.BioFactory(author=a)
+
+    def test_view_exists_at_desired_location(self):
+        resp = self.client.get('/blog/bloggers/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_accessible_by_name(self):
+        resp = self.client.get(reverse('blog:author_list'))
+        self.assertEqual(resp.status_code, 200)
+
+
+class AuthorDetailTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        for anum in range(13):
+            a = factories.AuthorFactory()
+            factories.BioFactory(author=a)
+            for bpnum in range(19):
+                bp = factories.BlogPostFactory(author=a)
+
+    def test_view_exists_at_desired_location(self):
+        resp = self.client.get('/blog/blogger/4/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_accessible_by_name(self):
+        resp = self.client.get(reverse('blog:author_detail', args=[4]))
+        self.assertEqual(resp.status_code, 200)
