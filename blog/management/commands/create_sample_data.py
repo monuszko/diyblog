8654c7d3ff0c2fee9bb3c2f65d381d65a5a1aa00
Marek Onuszko <marek.onuszko@gmail.com>
2018-02-05 14:19:54 +0100

    optimized management command

--- blog/management/commands/create_sample_data.py
+++ blog/management/commands/create_sample_data.py
@@ -1,30 +1,39 @@
 from django.core.management.base import BaseCommand
+from django.db import connection
 
 from blog import factories
-from blog.models import BlogPost, Author
-
+from blog.models import BlogPost, Author, Comment, Bio
 
 class Command(BaseCommand):
     help = "Populates the database with sample data to illustrate the app."
 
     def handle(self, *args, **options):
-        for x in range(9):
-            a = factories.AuthorFactory()
-            if a.pk < 7:
-                factories.BioFactory(author=a)
+        # Can't use bulk_create for these, because they need
+        # to be assigned to 'Bloggers' group.
+        authors = factories.AuthorFactory.create_batch(9)
+        bios = [factories.BioFactory.build(author=au) for au in authors[:7]]
+        Bio.objects.bulk_create(bios)
 
-        authors = Author.objects.all()
-        bool(authors)
         num_posts = 37
-        for y in range(num_posts):
-            factories.BlogPostFactory(author=authors[y % authors.count()])
+        posts = [
+                factories.BlogPostFactory.build(
+                    author=authors[np % len(authors)]
+                    )
+                for np in range(num_posts)
+                ]
+        BlogPost.objects.bulk_create(posts)
 
         posts = BlogPost.objects.all()
-        bool(posts)
         num_comments = 241
-        for y in range(num_comments):
-            factories.CommentFactory(
+        comments = [
+                factories.CommentFactory.build(
                     author=authors[y % len(authors)],
-                    blogpost=posts[y % len(posts)])
+                    blogpost=posts[y % len(posts)]
+                    )
+                    for y in range(num_comments)
+                ]
+
+        Comment.objects.bulk_create(comments)
+        print("Queries executed:", len(connection.queries))
 
 
